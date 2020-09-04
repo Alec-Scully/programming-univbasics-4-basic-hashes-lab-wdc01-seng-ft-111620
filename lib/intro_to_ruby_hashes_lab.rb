4c7b467919e152fa014031c19bbc996ec3968ae3
Alec Scully <scullyalec@gmail.com>
2020-09-04 00:03:51 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,14 +3,14 @@ def new_hash
 end
 
 def my_hash
-  has = {:name => "Alec", :age => 25}
+  hash = {:name => "Alec", :age => 25}
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  hash = {:name => "Grace Hopper"}
 end
 
 
 def id_hash_generator(number)
-  # return a hash with a key :id assigned to the provided number
+  hash = {:id => number}
 end
